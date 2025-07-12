fb366c8dbab6d6e1cb983e2f93b1db951353c374
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 00:42:26 +0600

    feat : Implement CashPayment class tightly coupled with OrderService, breaking DIP

--- src/Bad_Code/dip/CashPayment.java
+++ src/Bad_Code/dip/CashPayment.java
@@ -1,7 +1,8 @@
 package Bad_Code.dip;
 
 public class CashPayment {
-    public void pay(double amount) {
+    public void pay(double amount) 
+    {
         System.out.println("Paid in cash: " + amount);
     }
 }
