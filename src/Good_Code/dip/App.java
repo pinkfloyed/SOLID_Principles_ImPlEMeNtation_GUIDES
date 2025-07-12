327dc07cf5502169bfbc5f5164b8c78b07229794
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 00:45:37 +0600

    feat : implement DIP-compliant OrderService using abstraction and dependency injection

--- src/Good_Code/dip/App.java
+++ src/Good_Code/dip/App.java
@@ -1,7 +1,8 @@
 package Good_Code.dip;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         DipOrderService processor = new UpiPayment();
         OrderService service = new OrderService(processor);
         service.checkout(100.0);
