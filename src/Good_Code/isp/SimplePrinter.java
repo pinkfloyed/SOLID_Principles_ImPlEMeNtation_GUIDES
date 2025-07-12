d2a42f02ccd85fedd85adee0eb99eb7ff0a037d2
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 00:54:31 +0600

    feat : Add SimplePrinter implementing Printer interface with print functionality

--- src/Good_Code/isp/SimplePrinter.java
+++ src/Good_Code/isp/SimplePrinter.java
@@ -1,7 +1,9 @@
 package Good_Code.isp;
 
-public class SimplePrinter implements Printer {
-    public void print() {
+public class SimplePrinter implements Printer 
+{
+    public void print() 
+    {
         System.out.println("Printing...");
     }
 }
