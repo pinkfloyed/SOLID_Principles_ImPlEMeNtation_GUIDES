84af2bf8a1364b1c5893d5a22c47727bf1ede7f7
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 00:37:16 +0600

    feat : Add PaymentService demonstrating OCP violation by using conditional statements for payment types
    - Added PaymentService with if-else conditional logic violating Open/Closed Principle

--- src/Bad_Code/ocp/PaymentService.java
+++ src/Bad_Code/ocp/PaymentService.java
@@ -2,11 +2,16 @@ package Bad_Code.ocp;
 
 public class PaymentService {
     public void pay(String type, double amount) {
-        if (type.equals("cash")) {
+        if (type.equals("cash")) 
+        {
             System.out.println("Paid in cash: " + amount);
-        } else if (type.equals("credit")) {
+        } 
+        else if (type.equals("credit")) 
+        {
             System.out.println("Paid by credit card: " + amount);
-        } else {
+        } 
+        else 
+        {
             System.out.println("Unknown payment type.");
         }
     }
