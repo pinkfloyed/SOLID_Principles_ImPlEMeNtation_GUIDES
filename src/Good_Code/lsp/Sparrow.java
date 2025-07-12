7cdc064af0f81b0b1edd032c43be7dcc87bbb5f9
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 01:02:18 +0600

    feat : Implement Sparrow class extending Bird with flying behavior for LSP compliance

--- src/Good_Code/lsp/Sparrow.java
+++ src/Good_Code/lsp/Sparrow.java
@@ -1,7 +1,9 @@
 package Good_Code.lsp;
 
-public class Sparrow extends Bird {
-    public void move() {
+public class Sparrow extends Bird 
+{
+    public void move() 
+    {
         System.out.println("Sparrow is flying");
     }
 }
