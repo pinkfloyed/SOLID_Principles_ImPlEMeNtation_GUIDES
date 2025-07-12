87b198199d94ecd834fc8394c8fdabe0f83aeb9b
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 00:35:21 +0600

    feat : Add RestaurantService demonstrating SRP violation by combining multiple responsibilities
    - Added RestaurantService class violating SRP with order, cooking, and invoicing methods

--- src/Bad_Code/srp/RestaurantService.java
+++ src/Bad_Code/srp/RestaurantService.java
@@ -1,15 +1,18 @@
 package Bad_Code.srp;
 
 public class RestaurantService {
-    public void takeOrder(String order) {
+    public void takeOrder(String order) 
+    {
         System.out.println("Taking order: " + order);
     }
 
-    public void cookOrder(String order) {
+    public void cookOrder(String order) 
+    {
         System.out.println("Cooking order: " + order);
     }
 
-    public void generateInvoice(String order) {
+    public void generateInvoice(String order) 
+    {
         System.out.println("Generating invoice for: " + order);
     }
 }
