d5c3974c68f1c82e891f84a46fb9a87df7cfbe06
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-07-13 01:01:45 +0600

    feat : Implement Penguin class extending Bird with walking behavior for LSP compliance

--- src/Good_Code/lsp/Penguin.java
+++ src/Good_Code/lsp/Penguin.java
@@ -1,7 +1,9 @@
 package Good_Code.lsp;
 
-public class Penguin extends Bird {
-    public void move() {
+public class Penguin extends Bird 
+{
+    public void move() 
+    {
         System.out.println("Penguin is walking");
     }
 }
